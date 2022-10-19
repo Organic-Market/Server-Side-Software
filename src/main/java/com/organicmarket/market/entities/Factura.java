e6d3d66929668fa599861c4438169e7ff345f9c1
Luis Broncano <luiso2.2001b@gmail.com>
2022-10-19 18:50:31 -0500

    Creacion de Clases

--- src/main/java/com/organicmarket/market/entities/Factura.java
+++ src/main/java/com/organicmarket/market/entities/Factura.java
@@ -1,7 +1,16 @@
 package com.organicmarket.market.entities;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private float price;
+    private Date created;
+
+
+
 }
--- src/main/java/com/organicmarket/market/entities/Pedido.java
+++ src/main/java/com/organicmarket/market/entities/Pedido.java
@@ -1,7 +1,13 @@
 package com.organicmarket.market.entities;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Date created;
+
 }
