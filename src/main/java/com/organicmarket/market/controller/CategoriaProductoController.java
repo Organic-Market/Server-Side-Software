2e122fe5a8d79f52d73928bae4838e5417ec1020
Luis Broncano <luiso2.2001b@gmail.com>
2022-11-17 15:52:52 -0500

    Update

--- src/main/java/com/organicmarket/market/controller/CategoriaProductoController.java
+++ src/main/java/com/organicmarket/market/controller/CategoriaProductoController.java
@@ -1,5 +1,6 @@
 package com.organicmarket.market.controller;
 
+import com.organicmarket.market.Util.CategoriaExcelExporter;
 import com.organicmarket.market.entities.CategoriaProducto;
 import com.organicmarket.market.exception.ResourceNotFoundException;
 import com.organicmarket.market.repository.CategoriaProductoRepository;
@@ -8,7 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -40,4 +43,41 @@ public class CategoriaProductoController {
         categoriaProductoRepository.save(newCategoriaProducto);
         return new ResponseEntity<CategoriaProducto>(newCategoriaProducto, HttpStatus.CREATED);
     }
+
+    @Transactional
+    @PutMapping("/categoria/{id}")
+    public ResponseEntity<CategoriaProducto> update(@RequestBody CategoriaProducto categoriaProducto, @PathVariable Long id) {
+        CategoriaProducto categoriaProductoUpdate= categoriaProductoRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Not found category with id="+id));
+
+        categoriaProductoUpdate.setName(categoriaProducto.getName());
+
+        return new ResponseEntity<CategoriaProducto>(categoriaProductoRepository.save(categoriaProductoUpdate),
+                HttpStatus.OK);
+    }
+
+    @Transactional
+    @DeleteMapping("/categoria/{id}")
+    public ResponseEntity<HttpStatus> deleteCategory(@PathVariable("id") Long id){
+        categoriaProductoRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/categoria/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/octet-stream");
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=result_category";
+        response.setHeader(headerKey, headerValue);
+
+        List<CategoriaProducto> categoryResponse = categoriaProductoRepository.findAll();
+
+        CategoriaExcelExporter excelExporter = new CategoriaExcelExporter(
+                categoryResponse);
+
+        excelExporter.export(response);
+    }
+
 }
